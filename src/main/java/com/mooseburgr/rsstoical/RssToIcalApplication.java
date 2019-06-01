8395e277af804f5033642b5bc10cb6bdbe416b26
Kyle Johnson <kylejohnson.mn@gmail.com>
2019-06-01 13:32:12 -0500

    Add controller to convert RSS to iCal content

---
+++ src/main/java/com/mooseburgr/rsstoical/RssToIcalApplication.java
@@ -0,0 +1,14 @@
+package com.mooseburgr.rsstoical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RssToIcalApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssToIcalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mooseburgr/rsstoical/config/AppContext.java
@@ -0,0 +1,8 @@
+package com.mooseburgr.rsstoical.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppContext {
+
+}
